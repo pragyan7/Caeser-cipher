88a443d9a6c40018e38fa5c89fa6715bd7a65695
Pragyan Dash <68701602+pragyan7@users.noreply.github.com>
2024-06-04 00:48:32 +0530

    Create main.py
    This is the main code file

---
+++ main.py
@@ -0,0 +1,36 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    #Keep the number/symbol/space when the text is encoded/decoded?
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+#Import and print the logo from art.py when the program starts.
+from art import logo
+print(logo)
+
+#Ask the user if they want to restart the cipher program
+#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
+#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again
+should_continue = True
+while should_continue:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  #Add some code so that the program continues to work even if the user enters a shift number greater than 26. 
+  shift = shift % 26
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  result = input("Type 'yes' to continue, otherwise type 'no'.\n")
+  if result == "no":
+    should_continue = False
+    print("Goodbye, friend.")
